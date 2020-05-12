190dbc3a3ad6a17cc688374e333c8afb09f16d60
ToM <tomtom637@gmail.com>
2020-05-12 07:47:40 +0200

    Updates

---
+++ precache-manifest.e6ee1a153eecc9a672ded4b992a9fb57.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d28c7a1e24fc4415360f01ae22884ae",
+    "url": "/mastermind/index.html"
+  },
+  {
+    "revision": "e9d9915d79a272dc3f6f",
+    "url": "/mastermind/static/css/main.1431111b.chunk.css"
+  },
+  {
+    "revision": "6f1f89afa058c0733e7e",
+    "url": "/mastermind/static/js/2.4d1d23f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mastermind/static/js/2.4d1d23f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d9915d79a272dc3f6f",
+    "url": "/mastermind/static/js/main.7e22cd6f.chunk.js"
+  },
+  {
+    "revision": "c3cab64ceb392b02909a",
+    "url": "/mastermind/static/js/runtime-main.243d0c37.js"
+  },
+  {
+    "revision": "07c4bbe14a6854b7fb8327eb18c268d6",
+    "url": "/mastermind/static/media/barnacles.07c4bbe1.png"
+  },
+  {
+    "revision": "981fd42c5131744a6772df6cf4ef1df9",
+    "url": "/mastermind/static/media/dashi.981fd42c.png"
+  },
+  {
+    "revision": "4fcba85b65a7d9369746eeda50b8c78c",
+    "url": "/mastermind/static/media/inkling.4fcba85b.png"
+  },
+  {
+    "revision": "e76bbddfae52c3c5b13f45e50e0da19d",
+    "url": "/mastermind/static/media/kwazii.e76bbddf.png"
+  },
+  {
+    "revision": "ad7986a2424be9a9362134df00a46988",
+    "url": "/mastermind/static/media/peso.ad7986a2.png"
+  },
+  {
+    "revision": "fd3229d1cc326ba68322f5675fbe9b5f",
+    "url": "/mastermind/static/media/shellington.fd3229d1.png"
+  },
+  {
+    "revision": "7d513f8370c12711e2d7e5eb25ea22b1",
+    "url": "/mastermind/static/media/tunip.7d513f83.png"
+  },
+  {
+    "revision": "29b0fa3b9c5fc8d0cf078734019c6b54",
+    "url": "/mastermind/static/media/tweak.29b0fa3b.png"
+  }
+]);
